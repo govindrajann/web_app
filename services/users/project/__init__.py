e2ab3077bb1954cbf7d6bf126bf54521db787844
Govind rajan Narayanan <1097955+govindrajann@users.noreply.github.com>
2019-04-15 07:16:43 -0400

    added  project fiels

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,16 +1,39 @@
 # services/users/project/__init__.py
 
-from flask import Flask,jsonify
+
+import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy  # new
+
 
 # instantiate the app
-app  = Flask(__name__)
+app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
-@app.route('/users/ping',methods=['GET'])
+# instantiate the db
+db = SQLAlchemy(app)  # new
+
+
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+# routes
+@app.route('/users/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
-        'status':'Success!',
-        'message':'Pong!!'
+        'status': 'success',
+        'message': 'pong!'
     })
